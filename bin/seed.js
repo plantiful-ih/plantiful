ac57fadbe67028fc7847cea7f532e6698462e4d5
Ale Bausa <ale.bausa@gmail.com>
2019-09-09 07:32:49 +0200

    Closes:#16 Plant model done

---
+++ bin/seed.js
@@ -0,0 +1,2 @@
+const mongoose = require("mongoose");
+const Plant = require("../models/Plant");
---
+++ models/Plant.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const plantSchema = new Schema(
+    {
+        commonName: String,
+        family: String,
+        careDifficulty: Number,
+        watering: String,
+        lifeLength: String,
+        img: String,
+        careTips: Array,
+        place: Array,
+    },
+    {
+        timestamps: {
+            createdAt: 'created_at',
+            updatedAt: 'updated_at',
+        },
+    },
+);
+
+const Plant = mongoose.model('Plant', plantSchema);
+
+module.exports = Plant;
