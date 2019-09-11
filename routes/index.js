a9fd7ae80076589887d0a2c5bbc2b35a1e7758a3
Tash Tenner <tashfonetikas@gmail.com>
2019-09-11 14:25:49 +0200

    #8-#15-profile-get-view

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 const data = require('../data');
 const Plant = require('../models/Plant');
+const User = require('../models/User');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -17,6 +18,22 @@ router.get('/', (req, res, next) => {
     .catch((err) => {
       console.log('Error while displaying homepage', err);
     });
+});
+
+router.get('/profile', (req, res, next) => {
+  // const { userId } = req.params;
+
+  User.findById()
+    .then((user) => {
+      if (user) {
+        res.render('profile', { title: 'profile', user: req.user });
+      } else {
+        const error = new Error('There seems to be a mistake');
+        Error.status = 404;
+        throw error;
+      }
+    })
+    .catch(next);
 });
 
 module.exports = router;
